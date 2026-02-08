5aab9a63407d7156847e247e984b726543ccd4d7
Natan Cardoso <natansc73@gmail.com>
2026-02-08 02:00:40 -0300

    vai

--- app/page.tsx
+++ app/page.tsx
@@ -1,80 +1,80 @@
-'use client';
+"use client";
 
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import ReactLenis from "lenis/react";
+import ReactLenis, { type LenisRef } from "lenis/react";
 import Image from "next/image";
 import { useEffect, useRef } from "react";
 
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
-  const lenisRef = useRef(null);
-  const containerRef = useRef(null);
+  const lenisRef = useRef<LenisRef | null>(null);
+  const containerRef = useRef<HTMLElement | null>(null);
 
   useEffect(() => {
-    function update(time) {
+    function update(time: number) {
       lenisRef.current?.lenis?.raf(time * 1000);
     }
 
-    lenisRef.current?.lenis?.on(
-      "scroll",
-      ScrollTrigger.update
-    );
-    
+    lenisRef.current?.lenis?.on("scroll", ScrollTrigger.update);
+
     gsap.ticker.add(update);
     gsap.ticker.lagSmoothing(0);
-          
+
     return () => gsap.ticker.remove(update);
-  }, [])
+  }, []);
 
   useGSAP(() => {
     const sections = document.querySelectorAll("section");
 
-    sections.forEach((section, index) => {
-      const container = section.querySelector(".container");
+    sections.forEach(
+      (section, index) => {
+        const container = section.querySelector(".container");
 
-      gsap.to(container, {
-        rotation: 0,
-        ease: "none",
-        scrollTrigger: {
+        gsap.to(container, {
+          rotation: 0,
+          ease: "none",
+          scrollTrigger: {
+            trigger: section,
+            start: "top bottom",
+            end: "top 20%",
+            scrub: true,
+          },
+        });
+
+        if (index === sections.length - 1) return;
+
+        ScrollTrigger.create({
           trigger: section,
-          start: "top bottom",
-          end: "top 20%",
-          scrub: true,
-        },
-      })
-
-      if (index === sections.length - 1) return;
-
-      ScrollTrigger.create({
-        trigger: section,
-        start: "bottom bottom",
-        end: "bottom top",
-        pin: true,
-        pinSpacing: false
-      })
-  }, {
-    scope: containerRef,
-    })
-  })
+          start: "bottom bottom",
+          end: "bottom top",
+          pin: true,
+          pinSpacing: false,
+        });
+      },
+      {
+        scope: containerRef,
+      },
+    );
+  });
 
   return (
     <>
       <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
-      
+
       <main ref={containerRef}>
         {/* Section 1 */}
         <section className="um">
-          <div className="container">
+          <div className="container max-w-full">
             <div className="col">
               <h1>Hello World</h1>
             </div>
             <div className="col">
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-                quos.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quos.
               </p>
             </div>
           </div>
@@ -82,17 +82,23 @@ export default function Home() {
 
         {/* Section 2 */}
         <section className="dois">
-          <div className="container">
+          <div className="container max-w-full">
             <div className="col">
               <div className="img">
-                <Image src="/file.svg" alt="Image" width={100} height={100} />
+                <Image
+                  src="/dois.png"
+                  alt="Image"
+                  width={673}
+                  height={1200}
+                  className="w-full"
+                />
               </div>
             </div>
             <div className="col">
               <h1>Lorem ipsum</h1>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-                quos.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quos.
               </p>
             </div>
           </div>
@@ -100,17 +106,17 @@ export default function Home() {
 
         {/* Section 3 */}
         <section className="tres">
-          <div className="container">
+          <div className="container max-w-full">
             <div className="col">
               <h1>Lorem ipsum</h1>
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-                quos.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quos.
               </p>
             </div>
             <div className="col">
               <div className="img">
-                <Image src="/file.svg" alt="Image" width={100} height={100} />
+                <Image src="/tres.png" alt="Image" width={1200} height={904} />
               </div>
             </div>
           </div>
@@ -118,9 +124,9 @@ export default function Home() {
 
         {/* Section 4 */}
         <section className="quatro">
-          <div className="container">
+          <div className="container max-w-full">
             <div className="img">
-              <Image src="/file.svg" alt="Image" width={100} height={100} />
+              <Image src="/quatro.png" alt="Image" width={900} height={1200} />
             </div>
             <h1>Lorem ipsum</h1>
             <p>
@@ -136,14 +142,14 @@ export default function Home() {
 
         {/* Section 5 */}
         <section className="cinco">
-          <div className="container">
+          <div className="container max-w-full">
             <div className="col">
               <h1>Hello World</h1>
             </div>
             <div className="col">
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-                quos.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quos.
               </p>
             </div>
           </div>
@@ -151,14 +157,14 @@ export default function Home() {
 
         {/* Section 6 */}
         <section className="seis">
-          <div className="container">
+          <div className="container max-w-full">
             <div className="col">
               <h1>Hello World</h1>
             </div>
             <div className="col">
               <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-                quos.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quisquam, quos.
               </p>
             </div>
           </div>
